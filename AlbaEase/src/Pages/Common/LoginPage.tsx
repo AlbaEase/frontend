5479d3796de0dcc73f92010b9be252d35851c8cc
JeongHyeon <0429cjhn@naver.com>
2025-02-02 22:04:44 +0900

    로그인, 회원가입 페이지 초안

--- AlbaEase/src/Pages/Common/LoginPage.tsx
+++ AlbaEase/src/Pages/Common/LoginPage.tsx
@@ -1,19 +1,59 @@
-import "./LoginPage.css";
+import styles from "./LoginPage.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+// interface FormData {
+//   id: string;
+//   password: string;
+// }
 
 const LoginPage = () => {
+  // const [formDate, setFormDate] = useState<FormData>({
+  //   id: "",
+  //   password: "",
+  // });
+
+  // id, password 상태관리
+  const [id, setId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    return setId(e.target.value);
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    return setPassword(e.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (id.trim() && password.trim()) {
+      try {
+        // api연동구간
+        navigate("/main");
+      } catch (error) {
+        alert("로그인 실패: 다시 시도해주세요.");
+      }
+    } else {
+      alert("아이디와 비밀번호를 입력해주세요.");
+    }
+  };
+  // 로그인 시 에러나 오류를 alert()로 만들었는데 -> 모달 또는 UI컴포넌트 기반 메시지로 바꾸기
+
   return (
-    <div className="loginPage">
-      <div className="content">
-        <div className="contentPosition">
+    <div className={styles.loginPage}>
+      <div className={styles.content}>
+        <div className={styles.contentPosition}>
           <div style={{ fontSize: "50px" }}>
             Sign in to
             <div style={{ fontSize: "35px" }}>AlbaEase</div>
           </div>
-          <div className="content-2">
+          <div className={styles.content2}>
             <div>
               만약, 알바이즈에 가입되어 있지 않으시다면
               <br />
-              <a>여기를 클릭</a>해 회원가입 하세요!!
+              <Link to="../register">여기를 클릭</Link>해 회원가입 하세요!!
             </div>
             <div>
               <img src="src/assets/AlbaEase_model.png" />
@@ -21,18 +61,29 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-      <div className="form">
-        <div className="loginForm">
+      <div className={styles.form}>
+        <div className={styles.loginForm}>
           <div style={{ fontSize: "30px" }}>Sign in</div>
           <div>
-            <input className="forms" type="email" placeholder="ID" />
+            <input
+              type="text"
+              value={id}
+              onChange={handleId}
+              placeholder="ID"
+            />
           </div>
           <div>
-            <input type="password" placeholder="Password" />
+            {/* 비밀번호 확인할 수 있는 기능 구현하기 */}
+            <input
+              type="password"
+              value={password}
+              onChange={handlePassword}
+              placeholder="Password"
+            />
           </div>
-          <div className="fontStyle">비밀번호를 잃어버리셨나요?</div>
+          <div className={styles.fontStyle}>비밀번호를 잃어버리셨나요?</div>
           <div style={{ marginTop: "40px" }}>
-            <button>Login</button>
+            <button onClick={handleLogin}>Login</button>
             <p className="fontStyle" style={{ textAlign: "center" }}>
               or
             </p>
@@ -52,3 +103,6 @@ export default LoginPage;
 // 2. 글꼴 확인한 후 변경하기
 // 3. loginForm 색상 변경하기 -> 정확하게 넣지 못함 일단 비슷하게 임의로 넣었는데 다음과 같이 넣어도 되는건지 잘 모르겠음
 // 4. 현재 html, css 작업만 한 상태 앞으로 타입스크립로 문법 짜서 넣기
+
+//  - 아이디랑 비밀번호 조건이 어떻게 되는지
+//  조건을 정해야 할 거 같다.
--- AlbaEase/src/Pages/Common/RegisterPage.tsx
+++ AlbaEase/src/Pages/Common/RegisterPage.tsx
@@ -1,7 +1,275 @@
-import "./RegisterPage.css";
+import styles from "./RegisterPage.module.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const RegisterPage = () => {
-  return <>회원가입페이지를 구현할 예정입니다.</>;
+  // 회원가입 과정에서 사용하는 이름, 전화번호, 인증번호, 아이디, 비밀번호, 비밀번호 확인
+  const [userName, setUserName] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+  const [id, setId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordCheck, setPasswordCheck] = useState<string>("");
+
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+  const handlePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.target.value);
+  };
+  const handleCode = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCode(e.target.value);
+  };
+  const handleId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  const handlePasswordCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordCheck(e.target.value);
+  };
+  // 라디오 선택 상태변수 초기값은 선택되지 않은 false로 설정
+  const [isRadioSelect, setIsRadioSelect] = useState<boolean>(false);
+  // 회원가입 절차를 step으로 상태변수 관리 1페이지부터 시작이라서 초기값 1로 설정
+  const [step, setStep] = useState<number>(1);
+
+  // 라디오 핸들 함수
+  const handleRadio = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsRadioSelect(e.target.checked);
+  };
+  // 버튼에서 사용할 기능 라디오 버튼을 체크하였을 경우만 다음 스테이지로 넘어갈 수 있도록
+  const handleNext = () => {
+    if (isRadioSelect) {
+      setStep((prevStep) => prevStep + 1);
+    } else {
+      alert("라디오 상자를 선택");
+    }
+  };
+  // alert로 알람창이 뜨고 있는데 -> 모달창 또는 효과로 고쳐 나가기
+
+  return (
+    <div className={styles.registerPage}>
+      <div className={styles.content}>
+        <div className={styles.contentPosition}>
+          <div style={{ fontSize: "50px" }}>
+            Sign Up to
+            <div style={{ fontSize: "35px" }}>AlbaEase</div>
+          </div>
+          <div className={styles.content2}>
+            <div>
+              이미 알바이즈 계정이 있으시다면
+              <br />
+              <Link to="../login">여기를 클릭</Link>해 로그인 하세요!
+            </div>
+            <div>
+              <img src="src/assets/AlbaEase_model.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.form}>
+        <div className={styles.registerForm}>
+          {/* 스텝 1 */}
+          {step === 1 && (
+            <>
+              <div style={{ fontSize: "30px" }}>Sign up</div>
+              <div className={styles.registerText}>
+                알바이즈 가입을 위헤
+                <br />
+                카카오 가입 시에도 꼭 선택해 주세요!
+              </div>
+              <div className={styles.radioSelect}>
+                <div>
+                  <input
+                    className={styles.input}
+                    type="radio"
+                    name="role"
+                    id="owner"
+                    onChange={handleRadio}
+                  />
+                  사장님
+                </div>
+                <div>
+                  <input
+                    className={styles.input}
+                    type="radio"
+                    name="role"
+                    id="employee"
+                    onChange={handleRadio}
+                  />
+                  알바생
+                </div>
+              </div>
+              <div className={styles.registerText} style={{ fontSize: "22px" }}>
+                ----- 가입 방법 선택 -----
+              </div>
+              <div style={{ marginTop: "40px" }}>
+                <button onClick={handleNext}>알바이즈에 가입하기</button>
+                <p className={styles.fontStyle} style={{ textAlign: "center" }}>
+                  or
+                </p>
+                <button style={{ backgroundColor: "yellow", color: "black" }}>
+                  카카오로 가입하기
+                </button>
+              </div>
+            </>
+          )}
+
+          {/* 스텝 2 */}
+          {step === 2 && (
+            <>
+              <div style={{ fontSize: "30px" }}>Sign up</div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="이름"
+                  value={userName}
+                  onChange={handleName}
+                />
+              </div>
+              <div>
+                <input
+                  type="tel"
+                  placeholder="전화번호"
+                  value={phoneNumber}
+                  onChange={handlePhoneNumber}
+                />
+              </div>
+              <button style={{ marginTop: "40px" }} onClick={handleNext}>
+                전화번호 인증하기
+              </button>
+            </>
+          )}
+
+          {/* 스텝 3 */}
+          {step === 3 && (
+            <>
+              <div style={{ fontSize: "30px" }}>Sign up</div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="이름"
+                  value={userName}
+                  onChange={handleName}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="전화번호"
+                  value={phoneNumber}
+                  onChange={handlePhoneNumber}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="인증번호"
+                  value={code}
+                  onChange={handleCode}
+                />
+              </div>
+              <button style={{ marginTop: "40px" }} onClick={handleNext}>
+                인증번호 확인
+              </button>
+              <div className={styles.fontStyle} style={{ textAlign: "center" }}>
+                인증번호 재전송
+              </div>
+            </>
+          )}
+
+          {/* 스탭 4 */}
+          {step === 4 && (
+            <>
+              <div style={{ fontSize: "30px" }}>Sign up</div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="이름"
+                  value={userName}
+                  onChange={handleName}
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="전화번호"
+                  value={phoneNumber}
+                  onChange={handlePhoneNumber}
+                />
+              </div>
+              <div
+                className={styles.fontStyle}
+                style={{ textAlign: "left", marginTop: "5px" }}
+              >
+                전화번호 인증완료
+              </div>
+              {/* 중복확인을 할 수 있는 내용을 추가해야함 */}
+              <div>
+                <input
+                  type="text"
+                  placeholder="아이디"
+                  value={id}
+                  onChange={handleId}
+                />
+              </div>
+              {/* '아이디'는 사용이 가능합니다. -> 중복확인(버튼)을 클릭했을 때 아래에 나올 수 있도록*/}
+              <div>
+                {/* 비밀번호 확인할 수 있는 기능 구현하기 */}
+                <input
+                  type="password"
+                  placeholder="비밀번호"
+                  value={password}
+                  onChange={handlePassword}
+                />
+              </div>
+              <div>
+                {/* 비밀번호 확인할 수 있는 기능 구현하기 */}
+                <input
+                  type="password"
+                  placeholder="비밀번호 확인"
+                  value={passwordCheck}
+                  onChange={handlePasswordCheck}
+                />
+              </div>
+              {/* 비밀번호가 틀립니다., 비밀번호가 일치합니다. -> 조건에 따라서 텍스트가 나올 수 있도록 구현 */}
+              <button style={{ marginTop: "40px" }} onClick={handleNext}>
+                알바이즈 가입하기
+              </button>
+            </>
+          )}
+
+          {/* 스탭 5 */}
+          {step === 5 && (
+            <>
+              <div style={{ fontSize: "30px" }}>Sign up</div>
+              약관 사항을 넣어야함
+              <button style={{ marginTop: "40px" }} onClick={handleNext}>
+                동의하고 가입하기
+              </button>
+            </>
+          )}
+
+          {/* 스탭 6, 7 선택사항인데 까다롭다. 조건을 라디오 박스에다가 넣어야 하는 건가. */}
+          {/* 스탭 6 */}
+          {step === 6 && (
+            <>
+              <div style={{ fontSize: "30px" }}>매장 등록하기(선택)</div>
+              <input
+                type="password"
+                placeholder="매장의 사업자 번호를 입력해 주세요"
+              />
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
+
+// 각 상태에다가 조건 부여하기 -> 글자수, 전화번호 등등 형식에 맞는지
+// 조건에 맞지 않을 경우 -> 오류 메세지 넣을 수 있도록 구현
+// 유효성 검사하기 -> ex) 다음 스텝으로 넘어갈 때 버튼이 input을 다 채워야지만 활성화 할 수 있도록 구현
