c86b005aac72955ae980b0aeb22fa533ca711bb1
JeongHyeon <0429cjhn@naver.com>
2025-02-07 10:52:39 +0900

    모달 틀, 내정보 틀

--- AlbaEase/src/components/Modal.tsx
+++ AlbaEase/src/components/Modal.tsx
@@ -1,7 +1,17 @@
 import styles from "./Modal.module.css";
 
 const Modal = () => {
-    return <div className={styles.modal}>Modal</div>;
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modal}>
+        <div className={styles.container} style={{ marginTop: "20px" }}>
+          <div className={styles.title}>근무 요청하기</div>
+          <div className={styles.button}>취소</div>
+        </div>
+        <div className={styles.content}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
--- AlbaEase/src/components/MyInformation.tsx
+++ AlbaEase/src/components/MyInformation.tsx
@@ -1,7 +1,48 @@
 import styles from "./MyInformation.module.css";
+import { useState } from "react";
 
 const MyInformation = () => {
-  return <div className={styles.MyInformation}>내정보를 넣을 예정입니다.</div>;
+  const [name, setname] = useState<string>("홍길동");
+  const [phone, setPhone] = useState<string>("010-1234-5678");
+  const [password, setPassword] = useState<string>("********");
+  const [job, setJob] = useState<string>("사장님");
+  const [place, setPlace] = useState<string>("GS25, 하노이맥주병거리");
+
+  const openModal = () => {};
+
+  return (
+    <div className={styles.MyInformation}>
+      <div
+        className={styles.contentContainer}
+        style={{ borderBottom: "1px black solid" }}
+      >
+        <div className={styles.title}>나의 정보</div>
+        <div className={styles.content} onClick={openModal}>
+          정보수정하기
+        </div>
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.title}>이름</div>
+        <div className={styles.content}>{name}</div>
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.title}>휴대폰 번호</div>
+        <div className={styles.content}>{phone}</div>
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.title}>비밀번호</div>
+        <div className={styles.content}>{password}</div>
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.title}>직업</div>
+        <div className={styles.content}>{job}</div>
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.title}>근무 매장</div>
+        <div className={styles.contentPlace}>{place}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MyInformation;
