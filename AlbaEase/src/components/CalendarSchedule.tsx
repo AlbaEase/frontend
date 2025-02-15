edbdf207c39f024e874f5952da498fa77775166c
ownue <enu_i@naver.com>
2025-02-15 18:38:06 +0900

    오류 수정

--- AlbaEase/src/components/CalendarSchedule.tsx
+++ AlbaEase/src/components/CalendarSchedule.tsx
@@ -1,51 +1,51 @@
 import styles from "./CalendarSchedule.module.css";
 
 interface CalendarScheduleProps {
-  openRequestModal: () => void; // 모달 열기 함수
+    schedules: Schedule[];
+    openRequestModal: () => void; // 모달 열기 함수
 }
 
 type Schedule = {
-  startTime: string;
-  endTime: string;
-  names: string[];
+    startTime: string;
+    endTime: string;
+    names: string[];
 };
 
 // props로 schedules 배열을 받음
-const CalendarSchedule = ({
-  schedules,
-  openRequestModal,
-}: {
-  schedules: Schedule[];
-  openRequestModal: () => void;
+const CalendarSchedule: React.FC<CalendarScheduleProps> = ({
+    schedules,
+    openRequestModal,
 }) => {
-  return (
-    <div className={styles.calendarSchedule} onClick={openRequestModal}>
-      {schedules.length > 0 ? (
-        schedules.map((group, index) => {
-          // "HH:mm:ss"에서 시와 분만 추출
-          const startTimeFormatted = group.startTime
-            .split(":")
-            .slice(0, 2)
-            .join(":");
-          const endTimeFormatted = group.endTime
-            .split(":")
-            .slice(0, 2)
-            .join(":");
+    return (
+        <div className={styles.calendarSchedule} onClick={openRequestModal}>
+            {schedules.length > 0 ? (
+                schedules.map((group, index) => {
+                    // "HH:mm:ss"에서 시와 분만 추출
+                    const startTimeFormatted = group.startTime
+                        .split(":")
+                        .slice(0, 2)
+                        .join(":");
+                    const endTimeFormatted = group.endTime
+                        .split(":")
+                        .slice(0, 2)
+                        .join(":");
 
-          return (
-            <div key={index} className={styles.scheduleItem}>
-              <div className={styles.workHours}>
-                {`${startTimeFormatted} ~ ${endTimeFormatted}`}
-              </div>
-              <div className={styles.names}>{group.names.join(", ")}</div>
-            </div>
-          );
-        })
-      ) : (
-        <div className={styles.noSchedule}>일정이 없습니다.</div>
-      )}
-    </div>
-  );
+                    return (
+                        <div key={index} className={styles.scheduleItem}>
+                            <div className={styles.workHours}>
+                                {`${startTimeFormatted} ~ ${endTimeFormatted}`}
+                            </div>
+                            <div className={styles.names}>
+                                {group.names.join(", ")}
+                            </div>
+                        </div>
+                    );
+                })
+            ) : (
+                <div className={styles.noSchedule}>일정이 없습니다.</div>
+            )}
+        </div>
+    );
 };
 
 export default CalendarSchedule;
