3d9ad51a5cb0f995a38420f60d55bb78ad8d809f
Jo Yu Sung <cys990617@gmail.com>
2025-02-23 17:11:31 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { resolve } from 'path'
+
+const __dirname = new URL('.', import.meta.url).pathname
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': resolve(__dirname, 'src')
     }
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://3.39.237.218:8080',  // ✅ 실제 백엔드 서버 주소
+        target: 'http://3.39.237.218:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
