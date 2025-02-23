7b275829fedcc69735f1382dc17ef6ab0bf89568
ownue <enu_i@naver.com>
2025-02-23 18:34:12 +0900

    step2 만드는 중

--- AlbaEase/src/components/Modal/RequestModal.tsx
+++ AlbaEase/src/components/Modal/RequestModal.tsx
@@ -16,6 +16,7 @@ interface CalendarScheduleProps {
 
 const RequestModal: React.FC<CalendarScheduleProps> = ({ onClose }) => {
     const [currentStep, setCurrentStep] = useState(1);
+    const [selectedName, setSelectedName] = useState("");
     const { currentDate } = useOwnerSchedule();
     const { modalData } = useModal();
 
@@ -25,12 +26,13 @@ const RequestModal: React.FC<CalendarScheduleProps> = ({ onClose }) => {
     const renderStepContent = () => {
         switch (currentStep) {
             case 1:
+                /* 공통 (시작) */
                 return (
                     <div className={styles.content}>
                         <div className={styles.category}>
                             <div className={styles.text}>요청 근무일자</div>
                             <div className={styles.schedule}>
-                                {currentDate.format("YYYY/MM/DD")} |
+                                {currentDate.format("YYYY/MM/DD")} |{" "}
                                 {modalData.length > 0 && (
                                     <span className={styles.scheduleList}>
                                         {modalData.map((group, index) => {
@@ -47,22 +49,21 @@ const RequestModal: React.FC<CalendarScheduleProps> = ({ onClose }) => {
                                                     .join(":");
 
                                             return (
-                                                <div
+                                                <span
                                                     key={index}
                                                     className={
                                                         styles.scheduleItem
                                                     }>
-                                                    <div
+                                                    <span
                                                         className={styles.time}>
                                                         {startTimeFormatted} -{" "}
                                                         {endTimeFormatted}
-                                                    </div>
-                                                </div>
+                                                    </span>
+                                                </span>
                                             );
                                         })}
                                     </span>
                                 )}
-                                {modalData.startTime}{" "}
                             </div>
                         </div>
                         <div className={styles.category}>
@@ -72,7 +73,7 @@ const RequestModal: React.FC<CalendarScheduleProps> = ({ onClose }) => {
                             </div>
                         </div>
                         <div className={styles.category}>
-                            <div className={styles.text}>요청 대상</div>
+                            <div className={styles.text1}>요청 대상</div>
                             <div className={styles.selectBox}>
                                 <label>
                                     <input type="checkbox" />
@@ -90,7 +91,65 @@ const RequestModal: React.FC<CalendarScheduleProps> = ({ onClose }) => {
                     </div>
                 );
             case 2:
-                return <div className={styles.content}>스텝 2 내용</div>;
+                /* 전체 요청 */
+                return (
+                    <div className={styles.content2}>
+                        <div className={styles.askText}>
+                            해당 근무 대체근무를 요청하시겠습니까?
+                        </div>
+                        <div className={styles.step2text}>
+                            <div className={styles.text2}>
+                                {"요청일자 : "}
+                                <span className={styles.schedule}>
+                                    {currentDate.format("YYYY/MM/DD")} |{" "}
+                                </span>
+                                {modalData.length > 0 && (
+                                    <span className={styles.scheduleList}>
+                                        {modalData.map(
+                                            (group: any, index: any) => {
+                                                // "HH:mm:ss"에서 시와 분만 추출
+                                                const startTimeFormatted =
+                                                    group.startTime
+                                                        .split(":")
+                                                        .slice(0, 2)
+                                                        .join(":");
+                                                const endTimeFormatted =
+                                                    group.endTime
+                                                        .split(":")
+                                                        .slice(0, 2)
+                                                        .join(":");
+
+                                                return (
+                                                    <span
+                                                        key={index}
+                                                        className={
+                                                            styles.scheduleItem
+                                                        }>
+                                                        <span
+                                                            className={
+                                                                styles.time
+                                                            }>
+                                                            {startTimeFormatted}{" "}
+                                                            ~ {endTimeFormatted}
+                                                        </span>
+                                                    </span>
+                                                );
+                                            }
+                                        )}
+                                    </span>
+                                )}
+                            </div>
+                            <div className={styles.text2}>
+                                {"기존 근무자 : "}
+                                {}
+                            </div>
+                            <div className={styles.text2}>{"요청 대상 : "}</div>
+                        </div>
+                        <div className={styles.confirm2} onClick={goToNextStep}>
+                            확인
+                        </div>
+                    </div>
+                );
             case 3:
                 return <div className={styles.content}>스텝 3 내용</div>;
             default:
@@ -105,7 +164,11 @@ const RequestModal: React.FC<CalendarScheduleProps> = ({ onClose }) => {
 
     return (
         <div className={styles.modalOverlay}>
-            <div className={styles.modal}>
+            {/* 모달창 사이즈 동적으로 조정 / modal, smallModal */}
+            <div
+                className={`${styles.modal} ${
+                    currentStep === 2 ? styles.smallModal : ""
+                }`}>
                 <div className={styles.container} style={{ marginTop: "20px" }}>
                     <div className={styles.title}>
                         {currentStep !== 3 ? "근무 요청하기" : "요청 완료"}
