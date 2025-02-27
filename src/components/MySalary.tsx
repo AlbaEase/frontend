b09139c7d4e6ae5f0d190f7d53acfdd08af9edc4
JeongHyeon <0429cjhn@naver.com>
2025-02-27 23:57:22 +0900

    마이페이지 수정중

--- src/components/MySalary.tsx
+++ src/components/MySalary.tsx
@@ -1,6 +1,23 @@
+import Calendar from "./Calendar";
 import styles from "./MySalary.module.css";
+import { useState } from "react";
+import MypageCalendar from "./MypageCalendar";
 
 const MySalary = () => {
-  return <div className={styles.MySalary}>MySalary 칸 입니다.</div>;
+  const [money, setMoney] = useState<string>("160,480");
+  return (
+    <div className={styles.MySalary}>
+      <div className={styles.contents1}>
+        <div className={styles.alba}>근무/급여 확인 - 김가윤</div>
+        <div className={styles.money}>{money}</div>
+      </div>
+      <div className={styles.contents2}>
+        <div className={styles.calendar}>
+          <MypageCalendar />
+        </div>
+        <div className={styles.albaRadio}></div>
+      </div>
+    </div>
+  );
 };
 export default MySalary;
